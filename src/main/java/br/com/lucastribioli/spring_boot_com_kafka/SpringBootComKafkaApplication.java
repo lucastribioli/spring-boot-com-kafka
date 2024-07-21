e6c1458521a093b6b10f40a3f616d114b31ea575
Lucas <luk.tribioli.nunes@gmail.com>
2024-07-21 11:18:51 -0300

    feat: initial project and added file docker-compose

---
+++ src/main/java/br/com/lucastribioli/spring_boot_com_kafka/SpringBootComKafkaApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucastribioli.spring_boot_com_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootComKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootComKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lucastribioli/spring_boot_com_kafka/SpringBootComKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucastribioli.spring_boot_com_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootComKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
